6aa62cca02bf826ae31e54c59ea823d22339ae85
OmarAlKhalaf <99047548@mydavinci.nl>
2022-03-20 15:33:57 +0100

    (refresh)

--- Papi Gelato.py
+++ Papi Gelato.py
@@ -1,10 +1,21 @@
 #===========================varibales hereunder =========================#
-balls = 1.10
+balls = 0.95
 amountb = 0
 cone = 1.25
 amountCone = 0
 trays = 0.75
 amountTrays =0   
+whippedcream = 0.50
+amountcream = 0
+sprinkles = 0.30
+amountsprinkles = 0
+caramelSauces_creen = 0.60
+amountcaramelsauces_creen = 0
+caramelsauces_trays = 0.90
+amountcaramelsauces_trays = 0
+No = "No extra costs"
+anser = None
+
 # ==========================functions here under==========================#
 def containers ():
     print("-------------------------------------------------")
@@ -13,27 +24,43 @@ def containers ():
 
 def sorry ():
     print("-------------------------------------")
-    print("Sorry, I don't understand that...")
+    print("Sorry dat is geen optie die we aanbieden...")
     print("_____________________________________")
-#============================== smaak function=============================#
-def smaak(x):
+#============================== flaever function=============================#
+def flaever(x):
      for x in range(x):
-        extra = input("Wat flavors do you want we have : \nA) Strawberry, \nC) Chocolate, \nM) Mint, \nV) Vanilla ? :")
-        if extra == "a":
-            extra = "Strawberry"
-        elif extra == "c":
-            extra = "Chocolate"
-        elif extra == "m":
-            extra = "Mint"
-        elif extra == "v":
-            extra = "Vanilla" 
+        flaever = input("Wat flavors do you want we have : \nA) Strawberry, \nC) Chocolate, \nM) Mint, \nV) Vanilla ? :") .lower()
+        if flaever == "a":
+            flaever = "Strawberry"
+        elif flaever == "c":
+            flaever = "Chocolate"
+        elif flaever == "m":
+            flaever = "Mint"
+        elif flaever == "v":
+            flaever = "Vanilla" 
+#================================ extra flaever's =======================#
+def extra_flaever(i):
+    global question,amountcream,amountsprinkles,amountcaramelsauces_creen,amountcaramelsauces_trays
+    for i in range(i):
+        question = input("What kind of topping do you want: \nA) None, \nB) Whipped cream, \nC) Sprinkles, \nD) Caramel Sauce in cone or \nE) Caramel Sauce in trays ?").lower()
+        if question == "a":
+            question = anser
+        elif question == "b":
+            amountcream +=1
+        elif question == "c":
+            amountsprinkles += 1
+        elif question == "d":
+            amountcaramelsauces_creen += 1 
+        elif question == "e":
+            amountcaramelsauces_trays += 1
+
 #=============================== repeat function ========================#
-def herhalen():
-    vraag = input('would u like to order again?(Y/N) ').lower()
+def again():
+    vraag = input('Would u like to order again?(Y/N) ').lower()
     if vraag == 'y':
         start()
     else:
-        factuur()
+        invoice()
 
 #=================================step 2 funtion =========================#
 def step2():
@@ -43,28 +70,31 @@ def step2():
         amountCone += 1
     elif stap2 == 'c':
         amountTrays += 1
-#================================= Factuur funtion ========================#
-def factuur():
+#================================= invoice funtion ========================#
+def invoice():
     global trays,amountTrays,balls,amountb,cone,amountCone
     print(f'Balls {amountb} x {1.10} Euro = {amountb*1.10:.2f}')
     print(f"Cone {amountCone} x {1.25} Euro = {amountCone*1.25:.2f}")
     print(f"Trays  {amountTrays} x {0.75} Euro = {amountTrays*0.75:.2f}")
-    print(f'your total is : {amountCone*cone+trays*amountTrays+balls*amountb:.2f}')
-    print("Thanks for coming, Hanve a nice day.")
+    print(f"Your total topping is {whippedcream*amountcream+sprinkles*amountsprinkles+caramelSauces_creen*amountcaramelsauces_creen+caramelsauces_trays*amountcaramelsauces_trays:.2f} ")
+    print(f'your total is : {amountCone*cone+trays*amountTrays+balls*amountb+whippedcream*amountcream+sprinkles*amountsprinkles+caramelSauces_creen*amountcaramelsauces_creen+caramelsauces_trays*amountcaramelsauces_trays:.2f}')
+    print("Thanks for coming, Have a nice day.")
 #=================================== start funtion =========================#
 def start():
     global amountb,balls,aantalBalls
     check = True
-    aantalBalls = int(input('how many balls  do u want ? '))
+    aantalBalls = int(input('How many balls  do u want ? '))
     amountb = amountb + aantalBalls
     while check == True:
 
         if aantalBalls <= 3: 
-            smaak(aantalBalls)
+            flaever(aantalBalls)
+            extra_flaever(aantalBalls)
             check = False
 
         elif aantalBalls <=8:
-            smaak(aantalBalls)
+            flaever(aantalBalls)
+            extra_flaever(aantalBalls)
             check = False
 
         elif aantalBalls > 8 :
@@ -73,7 +103,7 @@ def start():
         else:
             sorry()
     step2()
-    herhalen()
+    again()
 
 
 start()
